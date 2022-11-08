601bc3835f3967e0b7af388606801e85ada9b9bf
12nd09 <114971143+12nd09@users.noreply.github.com>
2022-11-08 15:23:38 +0800

    merge

--- resources/views/internship_view.blade.php
+++ resources/views/internship_view.blade.php
@@ -50,6 +50,7 @@
             </div>
         </div>
     </div>
+    
     <div id="defaultModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
         <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
             <!-- Modal content -->
