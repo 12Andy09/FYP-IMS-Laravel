1e2cfbf4edb66d934129fe080d57df52ce2d979b
tonny767 <86913937+tonny767@users.noreply.github.com>
2022-11-15 14:56:19 +0800

    changes on .env.example
    easier to fetch

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -12,13 +12,13 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <p><span class="font-bold text-base"> Name:</span> {{ $user->name }}</p>
-                    {{-- <p><span class="font-bold text-base"> Profile: </span>@if($user->student_profile->student_photo != null)<img src="{{ asset('profile/'.$user->student_profile->student_photo) }}" alt="Profile Photo" width="200" height="200"> @else Image Not Provided @endif</p>
+                    <p><span class="font-bold text-base"> Profile: </span>@if($user->student_profile->student_photo != null)<img src="{{ asset('profile/'.$user->student_profile->student_photo) }}" alt="Profile Photo" width="200" height="200"> @else Image Not Provided @endif</p>
                     <p><span class="font-bold text-base"> Name:</span> {{ $user->name }}</p>
                     <p><span class="font-bold text-base"> Student ID: </span>{{ $user->student_profile->student_id }}</p>
                     <p><span class="font-bold text-base"> Email: </span>{{ $user->email }}</p>
                     <p><span class="font-bold text-base"> Education: </span>{{ $user->student_profile->student_education }}</p>
                     <p><span class="font-bold text-base"> View Resume: </span> View </p>
-                    <p><span class="font-bold text-base"> AboutMe: </span>{{ $user->student_profile->student_aboutMe }}</p> --}}
+                    <p><span class="font-bold text-base"> AboutMe: </span>{{ $user->student_profile->student_aboutMe }}</p>
                 </div>
             </div>
         </div>
