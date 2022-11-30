8e40e50afde0589762b04ca7315773acade740a4
Andy <114971143+12nd09@users.noreply.github.com>
2022-11-30 13:41:55 +0800

    Update ChartJSController.php

--- app/Http/Controllers/ChartJSController.php
+++ app/Http/Controllers/ChartJSController.php
@@ -1,12 +1,12 @@
 <?php
-  
+
 namespace App\Http\Controllers;
-    
+
 use Illuminate\Http\Request;
 use App\Models\Application;
 use App\Models\Internship;
-use DB;
-    
+use Illuminate\Support\Facades\DB;
+
 class ChartJSController extends Controller
 {
     /**
@@ -16,21 +16,21 @@ class ChartJSController extends Controller
      */
     public function Charts(Request $request)
     {
-        $applications = Application::select(DB::raw("COUNT(*) as count"),DB::raw("application_status as status_name"))
-                        ->groupBy(DB::raw("status_name"))
-                        ->pluck('count','status_name');
+        $applications = Application::select(DB::raw("COUNT(*) as count"), DB::raw("application_status as status_name"))
+            ->groupBy(DB::raw("status_name"))
+            ->pluck('count', 'status_name');
 
-                        $labels = $applications ->keys();
-                        $data = $applications ->values();
-        
-        $internships = Internship::select(DB::raw("COUNT(*) as count"),DB::raw("internship_category_id as internship_categories"))
-                        ->groupBy(DB::raw("internship_categories"))
-                        ->orderBy('internship_category_id','ASC')
-                        ->pluck('count','internship_categories');
+        $labels = $applications->keys();
+        $data = $applications->values();
 
-                        $labels2 = $internships->keys();
-                        $data2 = $internships->values();
-                        
-    	return view('admin.viewReport',compact('labels','data', 'labels2','data2'));
+        $internships = Internship::select(DB::raw("COUNT(*) as count"), DB::raw("internship_category_id as internship_categories"))
+            ->groupBy(DB::raw("internship_categories"))
+            ->orderBy('internship_category_id', 'ASC')
+            ->pluck('count', 'internship_categories');
+
+        $labels2 = $internships->keys();
+        $data2 = $internships->values();
+
+        return view('admin.viewReport', compact('labels', 'data', 'labels2', 'data2'));
     }
 }
